ef270e250709c81d163d1f36c411018ad6f3f4c1
Ednaira Viana <ednairaviana.evs@gmail.com>
2026-04-30 19:18:13 -0300

    feat: create fetchData

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,21 @@
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    fetchData();
+
+    async function fetchData() {
+      try {
+        const search = "https://api.thecatapi.com/v1/images/search?limit=10";
+        const res = await fetch(search);
+        const data = await res.json();
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }, []);
+
   return <></>;
 }
 
